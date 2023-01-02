e5ecd9d9aec34b6a5eead85a81bb2effbd241402
Niccoló Matteo Borgato <borgato.niccolo@gmail.com>
2023-01-02 19:28:12 +0100

    Exercise creating ASCII art

---
+++ ASCII/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // ASCII art number 1
+        Console.WriteLine("THE GHOST OF THE FUTURE");
+        Console.WriteLine(" .-.");  
+        Console.WriteLine("(o o)");
+        Console.WriteLine("| O |");
+        Console.WriteLine("|   |");
+        Console.WriteLine("'~~~'");  
+
+        // ASCII art number 2
+        Console.WriteLine("\nME MYSELF AND I");
+        Console.WriteLine("c_     ___");  
+        Console.WriteLine("　 \\\\__|__|__");
+        Console.WriteLine("　　 \\( o_o)");
+        Console.WriteLine("　　　 > ~  >");
+        Console.WriteLine("　　　/ 　 / \\\\");  
+        Console.WriteLine("　　 /　　/　 \\\\");
+        Console.WriteLine("　　 \\　 )　　c/");
+        Console.WriteLine("　　/　 /");
+        Console.WriteLine("　 /　/|");
+        Console.WriteLine("　(　( \\");
+        Console.WriteLine("　|　|  \\");
+        Console.WriteLine("　| / \\  )");
+        Console.WriteLine("　| |　) |");
+        Console.WriteLine(" /  )(_/");
+        Console.WriteLine("(_ /");
+    }
+}
