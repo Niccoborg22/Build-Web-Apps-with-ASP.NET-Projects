88226a013a41ec05af8a4c68ffe7e6c035f6ec6a
Niccolo <borgato.niccolo@gmail.com>
2023-01-25 19:37:03 +0100

    Copied all the documents needed to do the exercise

--- Rover control center/Program.cs
+++ Rover control center/Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        
-    }
+        MoonRover lunokhod = new MoonRover("Lunokhod 1", 1970);
+        MoonRover apollo = new MoonRover("Apollo 15", 1971);
+        MarsRover sojourner = new MarsRover("Sojourner", 1997);
+        Satellite sputnik = new Satellite("Sputnik", 1957); 
+    }cd
 }
