f6ec3c8ebbcac241dc0bf875c4d2cb9e3485d10e
Niccolo <borgato.niccolo@gmail.com>
2023-01-28 11:58:31 +0100

    Copied all the files needed to start the exercise

--- Programming languages/Program.cs
+++ Programming languages/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        List<Language> languages = File.ReadAllLines("./languages.tsv")
+        .Skip(1)
+        .Select(line => Language.FromTsv(line))
+        .ToList();
     }
 }
