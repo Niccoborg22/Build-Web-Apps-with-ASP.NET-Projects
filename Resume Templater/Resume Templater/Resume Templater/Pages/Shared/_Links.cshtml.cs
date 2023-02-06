c94b65ab4a4d6c1a685bd00e484f717626dcb475
Niccolo <borgato.niccolo@gmail.com>
2023-02-06 10:17:06 +0100

    Exercise completed, still having an error that, according to CodeAcademy forums can't be fixed

---
+++ Resume Templater/Resume Templater/Resume Templater/Pages/Shared/_Links.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Resume_Templater.Pages.Shared
+{
+    public class _LinksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
