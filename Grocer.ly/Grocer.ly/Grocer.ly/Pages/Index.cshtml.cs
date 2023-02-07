a292e2fb7829e17bf07b741736a83d40b9d3c691
Niccolo <borgato.niccolo@gmail.com>
2023-02-07 20:01:06 +0100

    Created the files for the exercise

---
+++ Grocer.ly/Grocer.ly/Grocer.ly/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using GroceryStore.Models;
+
+namespace GroceryStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        [BindProperty]
+        public int Rating { get; set; }
+        [BindProperty]
+        public string Feedback { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+    }
+}
